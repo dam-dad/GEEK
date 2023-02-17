4602283f7cb79e91e89dc4f5ea4683eaa8afd00b
eguerod <eduardo.guerra.rguez@gmail.com>
2023-02-17 09:58:07 +0000

    Update AutoUpdateThread.java

--- src/main/java/dad/geek/utils/AutoUpdateThread.java
+++ src/main/java/dad/geek/utils/AutoUpdateThread.java
@@ -3,15 +3,15 @@ package dad.geek.utils;
 import dad.geek.controllers.MainController;
 
 public class AutoUpdateThread extends Thread {
-	private MainController controller;
+//	private MainController controller;
 	
 	public AutoUpdateThread(MainController controller) {
-		this.controller = controller;
+//		this.controller = controller;
 		}
 	
 	@Override
 	public synchronized void run() {
-		controller.reloadPosts();
+//		controller.reloadPosts();
 		super.run();
 	}
 }
